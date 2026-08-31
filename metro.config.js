12c5e29ddd57194d222bfd3bf4c1112208f338a0
SoumilS18 <soumils181107@gmail.com>
2026-08-31 23:19:53 +0530

    chore(build): declare babel-preset-expo devDependency and optimize metro config for cloud linux build

--- metro.config.js
+++ metro.config.js
@@ -5,6 +5,8 @@ const { getDefaultConfig } = require('expo/metro-config');
 const config = getDefaultConfig(__dirname);
 
 // In-process bundling ensures direct buffer passing for Expo Router context modules in Bun on Windows
-config.maxWorkers = 1;
+if (process.platform === 'win32') {
+  config.maxWorkers = 1;
+}
 
 module.exports = config;
